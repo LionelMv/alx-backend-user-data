9dcfc60a9f39e7a5db62ed26e75bc01d64358cdf
LionelMv <mvangicode@gmail.com>
2023-11-02 20:10:50 +0300

    Add filtered_logger file

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import re
+from typing import List
+
+
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    """
+    Obfuscate specified fields in a log message.
+
+    Args:
+    fields (List[str]): A list of strings representing fields to obfuscate.
+    redaction (str): A string representing the replacement
+        for obfuscated fields.
+    message (str): A string representing the log line to be obfuscated.
+    separator (str): A string representing the character that separates
+        fields in the log message.
+
+    Returns:
+    str: The obfuscated log message.
+    """
+    # pattern = '|'.join(fields)
+    # print(pattern)
+    # return re.sub(f'({pattern})=[^;]+', f'\\1={redaction}', message)
+
+    for field in fields:
+        replace = f"{field}={redaction}{separator}"
+        message = re.sub(fr"{field}=[\w/]*{separator}", replace, message)
+    return message
