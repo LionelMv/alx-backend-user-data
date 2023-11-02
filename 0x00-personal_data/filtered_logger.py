ffc5c18b32b6d9c14e78c10c93c754ae8404791d
LionelMv <mvangicode@gmail.com>
2023-11-02 22:53:25 +0300

    Add format method

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,6 +2,7 @@
 """This  module contains user data management"""
 import re
 from typing import List
+import logging
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
@@ -29,3 +30,24 @@ def filter_datum(fields: List[str], redaction: str, message: str,
         # message = re.sub(fr"{field}=[\w/]*{separator}", replace, message)
         message = re.sub(fr"{field}=.*?{separator}", replace, message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """Initialize class"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """this function filters values in incoming log records using
+         filter_datum function"""
+        message = super(RedactingFormatter, self).format(record)
+        return filter_datum(self.fields, self.REDACTION, message, 
+                            self.SEPARATOR)
