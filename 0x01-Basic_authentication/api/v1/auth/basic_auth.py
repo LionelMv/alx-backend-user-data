3ce3f2bcf7d0777c3bd312d5d97db53874fb605b
LionelMv <mvangicode@gmail.com>
2023-11-06 23:23:09 +0300

    Add basic_auth file

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Basic Authentication implementation module.
+"""
+from auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    Class to implement Basic Authentication.
+    """
+    pass
