0ce601f0cd1c5e7b80994773b26c9381152a78b3
LionelMv <mvangicode@gmail.com>
2023-11-07 22:19:29 +0300

    Add decode_base64_authorization_header method

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,7 @@ Basic Authentication implementation module.
 """
 from .auth import Auth
 from flask import request
+import base64
 
 
 class BasicAuth(Auth):
@@ -22,3 +23,18 @@ class BasicAuth(Auth):
             return None
         else:
             return authorization_header.split("Basic ")[1]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """
+        Returns the decoded value of a Base64 string.
+        """
+        if base64_authorization_header is None or \
+                type(base64_authorization_header) != str:
+            return None
+
+        try:
+            return base64.b64decode(
+                base64_authorization_header).decode("utf-8")
+        except Exception:
+            return None
