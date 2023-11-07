ab95ccf50e1a38f6ad4c4d3d1101284727ed666b
LionelMv <mvangicode@gmail.com>
2023-11-07 22:01:07 +0300

    Add extract_base64_authorization_header method

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,11 +2,19 @@
 """
 Basic Authentication implementation module.
 """
-from auth import Auth
+from .auth import Auth
+from flask import request
 
 
 class BasicAuth(Auth):
     """
     Class to implement Basic Authentication.
     """
-    pass
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        if authorization_header is None or type(authorization_header) != str:
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        else:
+            return authorization_header.split("Basic ")[1]
