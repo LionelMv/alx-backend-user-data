d4784ff372dba4be5cce6193024501947a294cf5
LionelMv <mvangicode@gmail.com>
2023-11-07 22:04:19 +0300

    Add docstring to the method

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -12,6 +12,10 @@ class BasicAuth(Auth):
     """
     def extract_base64_authorization_header(
             self, authorization_header: str) -> str:
+        """
+        Returns the Base64 part of the Authorzation header for
+        a Basic Authentication.
+        """
         if authorization_header is None or type(authorization_header) != str:
             return None
         if not authorization_header.startswith("Basic "):
