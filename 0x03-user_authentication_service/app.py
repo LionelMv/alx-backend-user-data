f3f818a270e09536ec9d6b48322b5fd48a263026
LionelMv <mvangicode@gmail.com>
2024-04-28 06:48:19 +0300

    add users route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """Flask app"""
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
+AUTH = Auth()
 app = Flask(__name__)
 
 
@@ -9,6 +11,24 @@ app = Flask(__name__)
 def index() -> str:
     """return a JSON payload of the form"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"])
+def users() -> str:
+    """
+        Implements the /users POST route.
+        The end-point should expect two form data fields:
+        "email" and "password".
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    # register user if user does not exist.
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"})
+    except Exception:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
